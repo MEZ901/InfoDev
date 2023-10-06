c530415013efb72fddfae63a1940c656e3962c04
faissal17 <faissalaoukacha@gmail.com>
2023-10-06 11:48:24 +0100

    add routes for authentification

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,2 +1,13 @@
 const express = require("express");
 
+const authRouter = express.Router();
+
+authRouter.get("/login", (req, res) => {
+  res.render("login");
+});
+
+authRouter.get("/register", (req, res) => {
+  res.send("register");
+});
+
+module.exports = authRouter;
