d113394b0157267f1d26765d8cb47aaa61dc0f7e
faissal17 <faissalaoukacha@gmail.com>
2023-10-08 23:37:08 +0100

    create login and register page front-end

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,11 +3,11 @@ const express = require("express");
 const authRouter = express.Router();
 
 authRouter.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { title: "Login Page" });
 });
 
 authRouter.get("/register", (req, res) => {
-  res.send("register");
+  res.render("register");
 });
 
 module.exports = authRouter;
