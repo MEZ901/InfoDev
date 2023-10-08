280bfe37fef3ab1785a88a761b05fd34213931f6
youssef tadlaoui <usftadlaoui@gmail.com>
2023-10-08 12:10:46 +0100

    i recovre data from db table user by prisma

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,6 +1,21 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma= new PrismaClient;
+
 class ProfileController{
-  static profile = (req,res,)=>{
-      res.render("profile",{title:"youssef"})
+  static async profile(req, res) {
+    console.log('first')
+    try {
+      // Use Prisma to fetch user data (replace with your specific query)
+      const users = await prisma.user.findMany();
+      // Pass the fetched data to the view
+      res.render('profile', { users });
+      console.log(users)
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send(`Internal Server Error:${error.message}`);
+    } 
+    console.log("here")
   }
 }
 
