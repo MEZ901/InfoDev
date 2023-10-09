87bfc08e7f0bfa9f84cb3f6d1bafdaf4e6b5d42a
youssef tadlaoui <usftadlaoui@gmail.com>
2023-10-09 16:22:26 +0100

    prepare modal pour recepere info

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -4,14 +4,13 @@ const prisma= new PrismaClient;
 
 class ProfileController{
   static async profile(req, res) {
-    console.log('first')
+  
     try {
       const userId=req.query.id
       const userIdInt = parseInt(userId);
-      // Use Prisma to fetch user data (replace with your specific query)
+      
       const users = await prisma.user.findUnique({ where: { id: userIdInt } });;
       const articles= await prisma.article.findMany({where: { authorId: userIdInt  }});
-      // Pass the fetched data to the view
       res.render('profile', { users ,articles});
       console.log(users)
       console.log(Object.keys(articles),"hello")
@@ -19,7 +18,7 @@ class ProfileController{
       console.error('Error:', error);
       res.status(500).send(`Error:${error.message}`);
     } 
-    console.log("here")
+   
   }
 }
 
